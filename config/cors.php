dd6155444e31f608b2dc4eb73777071769e7ff88
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-09-10 15:26:46 +0300

    edit in corse

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,14 @@ return [
 
   'allowed_origins_patterns' => [],
 
-  'allowed_headers' => ['Content-Type', 'X-Requested-With', 'X-CSRF-TOKEN', 'Authorization'],
+  'allowed_headers' => [
+    'Content-Type',
+    'X-Requested-With',
+    'X-CSRF-TOKEN',
+    'Authorization',
+    'X-Security-Block-Reason',
+    'X-Blocked-Phone',
+  ],
 
   'exposed_headers' => [],
 
