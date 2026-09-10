4495d64e917546fd3132660f94f2447527b23450
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-09-10 16:24:18 +0300

    added miuddleware corse in app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
   ->withMiddleware(function (Middleware $middleware): void {
 
     $middleware->append(SecurityHeadersMiddleware::class);
+    $middleware->append(\Illuminate\Http\Middleware\HandleCors::class);
 
 
     $middleware->alias([
